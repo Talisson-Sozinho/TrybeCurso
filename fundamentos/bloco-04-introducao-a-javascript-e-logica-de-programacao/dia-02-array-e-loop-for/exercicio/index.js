bedaa0473aee9e571ca97d4e390c62727c018060
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-08 18:56:11 -0300

    comando 05 do exercicio do bloco 4.2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicio/index.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicio/index.js
@@ -1,7 +1,10 @@
 // lista que será usada nesse execício
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let somaNumbers = 0;
-
+/* "Infinity" é uma variável global do js que significa o maio número
+* foi inicializada com o valor negativo para servir de menor número possível
+*/
+let maiorElemento = -Infinity;
 
 // Estrutura de repetição para percorrer a lista 
 for (let number of numbers) {
@@ -9,6 +12,10 @@ for (let number of numbers) {
   console.log('Elemento: ' + number);
   //fazendo uma soma cumulativa de todos os elemento da lista
   somaNumbers += number;
+  //Verificando qual é o maior elemento do array e guardando na variável maiorElemento
+  if ( number > maiorElemento){
+    maiorElemento = number;
+  }
 }
 // Mostrando no console a soma dos elementos da lista
 console.log('Soma de todos os elemento da lista: ' + somaNumbers);
@@ -19,10 +26,14 @@ const mediaAritmeticaArray = somaNumbers / numbers.length;
 // Mostrando no console a média aritmética de todos os elemento da lista
 console.log('Média aritmética dos elemento da lista: ' + mediaAritmeticaArray);
 
+// Fazendo a verificação se a média é meio ou menor igual a 20
 if (mediaAritmeticaArray > 20) {
   console.log("valor maior que 20");
 
 } else {
   console.log('valor menor ou igual a 20');
-  
+
 }
+
+// Mostrando o maior elemento da lista
+console.log('Maior elemento da lista é: ' + maiorElemento);
