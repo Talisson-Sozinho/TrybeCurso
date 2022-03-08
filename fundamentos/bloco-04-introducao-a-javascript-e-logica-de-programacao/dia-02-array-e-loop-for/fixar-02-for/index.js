b43182a24da4777c87ac15a8d07381edaa436c3c
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-08 16:13:30 -0300

    para fixar 02 execicio 01 do bloco 4.2 concluido

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/fixar-02-for/index.js
@@ -0,0 +1,6 @@
+// Execício 01
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for ( let index = 0; index < groceryList.length; index += 1 ) {
+  console.log('Grocery List: ' + groceryList[index] );
+}
