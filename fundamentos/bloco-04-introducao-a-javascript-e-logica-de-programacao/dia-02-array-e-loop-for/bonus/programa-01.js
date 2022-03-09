1b8235b8f2907fcd4c57848a04a283f65c143e62
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-08 21:17:42 -0300

    programa 01 do execicio bonus do bloco 4.2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/bonus/programa-01.js
@@ -0,0 +1,14 @@
+// Ordena valores de um Array em ordem crescente
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let index = 0; index < numbers.length - 1; index +=1){
+  for (let secondIndex = index + 1; secondIndex < numbers.length; secondIndex+=1){
+    if( numbers[index] > numbers[secondIndex] ){
+      let aux = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = aux;
+    }
+  }
+}
+
+console.log(numbers);
