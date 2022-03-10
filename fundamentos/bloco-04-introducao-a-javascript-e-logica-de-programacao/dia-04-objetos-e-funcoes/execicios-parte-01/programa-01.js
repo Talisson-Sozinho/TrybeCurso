7fb2c454cb6049f5ca70037dc81e1d65d4849102
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-10 18:57:22 -0300

    concluido o programa 01 do execicios parte 01 do bloco 4.4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/execicios-parte-01/programa-01.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/execicios-parte-01/programa-01.js
@@ -1,8 +1,17 @@
+//Objeto Base
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
+// Novo objeto seguindo o comando 05
+let novaInfo = {
+  personagem: 'Tio Patinhas',
+  origem: `Christmas on Bear Mountain, Dell's Four Color Comics #178`,
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+}
+
 // Comando 01
 console.log(`Bem-vindo(a) ${info.personagem}`);
 
@@ -17,4 +26,12 @@ for ( let key in info ) {
 // Comando 04
 for ( let key in info){
   console.log(info[key]);
+}
+
+for ( let key in info ) {
+  if ( key === 'recorrente' && info[key] === novaInfo[key]){
+    console.log("Ambos recorrente");
+  }else {
+    console.log(`${info[key]} e ${novaInfo[key]}`)
+  }
 }
