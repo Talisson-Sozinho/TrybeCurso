ed695b695505ba35f7e49fe29f3a5c249ba48f26
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-14 19:10:32 -0300

    bloco 5.1 - para fixar 02 - completo

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/fixar-02-seletores-de-elementos/index.js
@@ -0,0 +1,16 @@
+const paragraph = document.querySelector("#paragraph");
+paragraph.style.color = "red";
+
+const titulo = document.querySelector("#page-title");
+titulo.innerHTML = 'Harry potter';
+
+const secondParagraph = document.querySelector('#second-paragraph');
+secondParagraph.innerHTML = 'Estou alterando o segundo parágrafo pelo javascript'
+
+const subTitle = document.querySelector('#subtitle');
+subTitle.innerHTML = 'TUDO PELO JAVASCRIPT';
+
+const paragraphs = document.querySelectorAll('.paragraphText');
+paragraphs[0].style.backgroundColor = 'blue';
+
+document.querySelectorAll('h4')[0].style.color = 'green';
