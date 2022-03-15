2f9eef2dfb4184025309de44a7701bb8d6cbf6fe
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-14 21:00:44 -0300

    bloco 5.1 - execicio - template pronto

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/execicios-funcoes-de-manipulacao-do-dom/script.js
@@ -0,0 +1,5 @@
+/*  Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+ *  - document.getElementById()
+ *  - document.getElementsByClassName()
+ *  - document.getElementsByTagName()
+ */
