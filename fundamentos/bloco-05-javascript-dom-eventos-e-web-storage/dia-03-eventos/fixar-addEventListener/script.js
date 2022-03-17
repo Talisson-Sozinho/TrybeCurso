b0f78ac486d988a1c18b346b65210bc91c1d59a0
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-16 21:52:56 -0300

    bloco 5.3 - feito requisito 3 e 4

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/fixar-addEventListener/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/fixar-addEventListener/script.js
@@ -30,10 +30,21 @@ function destacaTech(event){
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener('keyup', inserirTexto);
+
+function inserirTexto(event) {
+  const ondeInserir = document.querySelector('.tech');
+  ondeInserir.innerHTML = event.target.value;
+  console.log(event)
+  if (event.keyCode === 13) {
+    event.target.value = '';
+  }
+}
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+myWebpage.addEventListener('dblclick', () => window.open("https://github.com/Talisson-Sozinho"));
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
