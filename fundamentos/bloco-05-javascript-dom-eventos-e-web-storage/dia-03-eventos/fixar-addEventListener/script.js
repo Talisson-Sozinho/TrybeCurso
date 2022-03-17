379ef42f8d2bddfed148779ec5e795d604e1949e
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-16 21:17:28 -0300

    bloco 5.3 - feito requisito 1 e 2

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/fixar-addEventListener/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/fixar-addEventListener/script.js
@@ -8,9 +8,25 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+/* Resposta: É que nele esta adicionado a classe .tech */
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+firstLi.addEventListener('click', destacaTech);
+secondLi.addEventListener('click', destacaTech);
+thirdLi.addEventListener('click', destacaTech);
+
+function destacaTech(event){
+  if (event.target.className === 'tech'){
+    event.target.className = '';
+  }else {
+    const elementoIrmao = event.target.parentElement.children;
+    for (let index = 0; index < elementoIrmao.length; index += 1){
+      elementoIrmao[index].className = '';
+    }
+    event.target.className = 'tech';
+  }
+}
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
