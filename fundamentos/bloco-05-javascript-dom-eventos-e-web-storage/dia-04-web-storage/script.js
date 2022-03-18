54e67907ed1e9b2a3d958be1f0f39918fbb1846f
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-18 17:17:27 -0300

    bloco 5.4 - adiciona as opcoes de configuracoes da pagina

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-web-storage/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-web-storage/script.js
@@ -1,3 +1,28 @@
-const configureFont = document.querySelector('.input-font-size');
+const configureFontColor = document.querySelector('.input-font-color');
+const configureFontSize = document.querySelector('.input-font-size');
+const configureBackgroundColor = document.querySelector('.input-background-color');
+const configureFontFamily = document.querySelector('.input-font-family');
 
-configureFont.addEventListener('input', ()=>{console.log(configureFont.value)})
+function adicionaConfigurationOnPage (){
+  configureFontColor.addEventListener('input', ()=>{
+    const newFontColor = configureFontColor.value;
+    document.querySelector('.article-container').style.color = newFontColor;
+  });
+
+  configureFontSize.addEventListener('input', ()=>{
+    const newFontSize = configureFontSize.value;
+    document.querySelector('.article-container').style.fontSize = newFontSize + 'px';
+  });
+
+  configureBackgroundColor.addEventListener('input', ()=>{
+    const newBackgroundColor = configureBackgroundColor.value;
+    document.querySelector('.article-container').style.backgroundColor = newBackgroundColor;
+  });
+
+  configureFontFamily.addEventListener('input', ()=>{
+    const newFontFamily = configureFontFamily.value;
+    document.querySelector('.article-container').style.fontFamily = newFontFamily;
+  })
+
+}
+adicionaConfigurationOnPage();
