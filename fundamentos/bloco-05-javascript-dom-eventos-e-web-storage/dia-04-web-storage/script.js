14383a5debdd3d71e1e3d48b997929edf5a4ebf6
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-18 22:08:51 -0300

    bloco 5.4 - feito refatorando mais ainda

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-web-storage/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-web-storage/script.js
@@ -6,12 +6,6 @@ const configureFontFamily = document.querySelector('.input-font-family');
 const configureReset =  document.getElementsByClassName('.reset-config')[0];
 
 /* Obejetos com a configuração original da pagina */
-let articleConfig = {
-  fontColor: '#000000',
-  fontSize: 16,
-  BackgroundColor: '#e7e9ea',
-  fontFamily: 'Fira Code',
-};
 
 /* Chamando a função que irá colocar as configurações antigas da pessoa, na página caso tenha */
 initialRender();
@@ -23,8 +17,16 @@ adicionaInteractiveConfigurationOnPage();
 function initialRender() {
   const oldConfig = localStorage.getItem('articleConfig');
   if (oldConfig !== null){
-    articleConfig = JSON.parse(oldConfig);
+    const articleConfig = JSON.parse(oldConfig);
     applyOldConfig(articleConfig);
+  } else {
+    const  articleConfig = {
+      fontColor: '#000000',
+      fontSize: 16,
+      backgroundColor: '#e7e9ea',
+      fontFamily: 'Fira code',
+    };
+    localStorage.setItem('articleConfig', articleConfig);
   }
 }
 
@@ -34,60 +36,75 @@ function applyOldConfig(oldConfigObject){
   document.querySelector('.article-container').style.color = oldConfigObject.fontColor;
   configureFontSize.value = oldConfigObject.fontSize;
   document.querySelector('.article-container').style.fontSize = oldConfigObject.fontSize +'px';
-  configureBackgroundColor.value = oldConfigObject.BackgroundColor;
-  document.querySelector('.article-container').style.backgroundColor = oldConfigObject.BackgroundColor;
+  configureBackgroundColor.value = oldConfigObject.backgroundColor;
+  document.querySelector('.article-container').style.backgroundColor = oldConfigObject.backgroundColor;
   configureFontFamily.value = oldConfigObject.fontFamily;
   document.querySelector('.article-container').style.fontFamily = oldConfigObject.fontFamily;
 }
 
 // função para adicionar os escutadores de eventos nos elementos interativos
 function adicionaInteractiveConfigurationOnPage (){
-  configureFontColor.addEventListener('input', ()=>{
-    const newFontColor = configureFontColor.value;
-    document.querySelector('.article-container').style.color = newFontColor;
-    articleConfig.fontColor = newFontColor;
-    adicionandoNoLocalStorage(articleConfig);
-  });
+  configureFontColor.addEventListener('input', () => modTextColor(configureFontColor.value));
 
-  configureFontSize.addEventListener('input', ()=>{
-    const newFontSize = configureFontSize.value;
-    document.querySelector('.article-container').style.fontSize = newFontSize + 'px';
-    articleConfig.fontSize = newFontSize;
-    adicionandoNoLocalStorage(articleConfig);
-  });
+  configureFontSize.addEventListener('input', ()=> modFontSize(configureFontSize.value));
 
-  configureBackgroundColor.addEventListener('input', ()=>{
-    const newBackgroundColor = configureBackgroundColor.value;
-    document.querySelector('.article-container').style.backgroundColor = newBackgroundColor;
-    articleConfig.BackgroundColor = newBackgroundColor;
-    adicionandoNoLocalStorage(articleConfig);
-  });
+  configureBackgroundColor.addEventListener('input', ()=> modBackgroundColor(configureBackgroundColor.value));
 
-  configureFontFamily.addEventListener('input', ()=>{
-    const newFontFamily = configureFontFamily.value;
-    document.querySelector('.article-container').style.fontFamily = newFontFamily;
-    articleConfig.fontFamily = newFontFamily;
-    adicionandoNoLocalStorage(articleConfig);
-  })
+  configureFontFamily.addEventListener('input', ()=> modFontFamily(configureFontFamily.value));
   
-  configureReset.addEventListener('click', ()=>{
-    configureFontColor.value = '#000000';
-    document.querySelector('.article-container').style.color = '#000000';
-    articleConfig.fontColor = '#000000';
-    configureFontSize.value = 16;
-    document.querySelector('.article-container').style.fontSize = 16 +'px';
-    articleConfig.fontSize = 16;
-    configureBackgroundColor.value = '#e7e9ea';
-    document.querySelector('.article-container').style.backgroundColor = '#e7e9ea';
-    articleConfig.BackgroundColor = '#e7e9ea';
-    configureFontFamily.value = 'Fira Code';
-    document.querySelector('.article-container').style.fontFamily = 'Fira Code';
-    articleConfig.fontFamily = 'Fira Code';
-    adicionandoNoLocalStorage(articleConfig);
-  })
+  configureReset.addEventListener('click', reset)
 }
 
 // função para adicionar o objeto de configuração no localStorage
-function adicionandoNoLocalStorage(newConfig) {
-  localStorage.setItem('articleConfig', JSON.stringify(newConfig))
+function adicionandoNoLocalStorage(chave, newConfig) {
+  const objectOfConfig = JSON.parse(localStorage.getItem('articleConfig'))
+  objectOfConfig[chave] = newConfig;
+  localStorage.setItem('articleConfig', JSON.stringify(objectOfConfig))
+}
+
+/* Funções para a modificação no artigo */
+
+// função para modificar cor do texto
+function modTextColor (newColor){
+  document.querySelector('.article-container').style.color = newColor;
+  adicionandoNoLocalStorage('fontColor', newColor);
+}
+
+// função para modificar o tamanho da fonte
+function modFontSize (newFontSize){
+  document.querySelector('.article-container').style.fontSize = newFontSize + 'px';
+  adicionandoNoLocalStorage('fontSize', newFontSize);
+}
+
+// função para modificar o cor do background
+function modBackgroundColor(newBackgroundColor){
+  document.querySelector('.article-container').style.backgroundColor = newBackgroundColor;
+  adicionandoNoLocalStorage('backgroundColor', newBackgroundColor);
+}
+
+// função para modificar a fontfamily
+function modFontFamily(newFontFamily){
+  document.querySelector('.article-container').style.fontFamily = newFontFamily;
+  adicionandoNoLocalStorage('fontFamily', newFontFamily);
+}
+
+// função para resetar as configurações para a config inicial
+function reset () {
+  configureFontColor.value = '#000000';
+  document.querySelector('.article-container').style.color = '#000000';
+  adicionandoNoLocalStorage( 'fontColor', '#000000');
+
+  configureFontSize.value = 16;
+  document.querySelector('.article-container').style.fontSize = 16 +'px';
+  adicionandoNoLocalStorage( 'fontSize', 16);
+
+  configureBackgroundColor.value = '#e7e9ea';
+  document.querySelector('.article-container').style.backgroundColor = '#e7e9ea';
+  adicionandoNoLocalStorage( 'backgroundColor', '#e7e9ea');
+
+  configureFontFamily.value = 'Fira code';
+  document.querySelector('.article-container').style.fontFamily = 'Fira code';
+  adicionandoNoLocalStorage( 'fontFamily', 'Fira code');
+
+  
 }
