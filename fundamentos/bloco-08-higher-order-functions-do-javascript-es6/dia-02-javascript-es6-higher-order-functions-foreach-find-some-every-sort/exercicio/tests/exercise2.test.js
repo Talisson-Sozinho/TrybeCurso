d8dd8ad30bf46188306fa8694b7b1033c067366c
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-09 20:18:15 -0300

    bloco 8.2 - feito o teste do exercise 02

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio/tests/exercise2.test.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio/tests/exercise2.test.js
@@ -2,6 +2,6 @@ const smallerName = require('../src/exercise2');
 
 describe('Teste para o exercício 02: ', () => {
   it('Verificar se a função "smallerName" retorna o nome do menor livro.', () => {
-    fail('Teste vazio!');
+    expect(smallerName()).toBe('Duna');
   });
 });
