830397bf18eb2321f802f75c32d15c51d365d938
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-09 20:52:48 -0300

    bloco 8.2 - feito o teste do exercise 03

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio/tests/exercise3.test.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio/tests/exercise3.test.js
@@ -2,6 +2,17 @@ const getNamedBook = require('../src/exercise3');
 
 describe('Teste para o exercício 03: ', () => {
   it('Verificar se a função "getNamedBook" retorna primeiro livro cujo nome possui 26 caracteres.', () => {
-    fail('Teste vazio!');
+    const expectedResult = {
+      author: {
+        birthYear: 1948,
+        name: 'George R. R. Martin',
+      },
+      genre: 'Fantasia',
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      releaseYear: 1991,
+    };
+    
+    expect(getNamedBook()).toEqual(expectedResult);
   });
 });
