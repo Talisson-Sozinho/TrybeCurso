5e15f6ff4ea0b61fd75f81313f402ecb3848ef90
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-11 22:27:28 -0300

    bloco 8.3 - feito teste do exercise 06

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/tests/exercise6.test.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/tests/exercise6.test.js
@@ -1,5 +1,13 @@
 const oldBooks = require('../src/exercise6');
+const books = require('../src/template-library');
 
 describe('Teste do exercise6: ', () => {
-  fail('teste vazio!!!');
+  it('Verifica se a função retorna um array com o nome de todos os livros com mais de 60 anos de publicação', () => {
+    const expectedResult = [
+      'O Senhor dos Anéis',
+      'Fundação',
+      'O Chamado de Cthulhu',
+    ];
+    expect(oldBooks(books)).toEqual(expectedResult);
+  });
 });
