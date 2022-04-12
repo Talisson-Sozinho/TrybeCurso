51f98aeec967e9e9e9c327d2bd1a38f7113a34ae
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-12 00:43:45 -0300

    bloco 8.3 - feito exercise6 - função "oldBooks"

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/src/exercise6.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/src/exercise6.js
@@ -1,5 +1,8 @@
-function oldBooks() {
-  // escreva seu código aqui
+const currentYear = 2022;
+
+function oldBooks(books, moreThanYearsAgo) {
+  const arrayOfOldsBooks = books.filter((book) => currentYear - book.releaseYear > moreThanYearsAgo);
+  return arrayOfOldsBooks.map(book => book.name);
 }
 
 module.exports = oldBooks;
