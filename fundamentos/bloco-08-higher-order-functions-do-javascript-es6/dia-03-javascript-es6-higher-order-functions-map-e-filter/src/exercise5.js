97676b9aadb0575b4a7b2acba5badc533ed17875
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-12 00:34:39 -0300

    bloco 8.3 - feito exercise5 - função "fantasyOrScienceFiction"

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/src/exercise5.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/src/exercise5.js
@@ -1,5 +1,10 @@
-function fantasyOrScienceFictionAuthors() {
-  // escreva seu código aqui
+const fantasyOrScienceFiction = require('./exercise3');
+
+function fantasyOrScienceFictionAuthors(books, arrayOfGenre) {
+  const booksFiltered = fantasyOrScienceFiction(books, arrayOfGenre);
+  const arrayOfAuthors = booksFiltered.map((book) => book.author.name);
+  arrayOfAuthors.sort();
+  return arrayOfAuthors;
 }
 
 module.exports = fantasyOrScienceFictionAuthors;
