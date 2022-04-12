3495394ee82b019a86cb68eaf61d13be199a9b14
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-12 19:55:53 -0300

    bloco 8.4 - feito o teste do exercise3

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-higher-order-functions-reduce/exercicio/tests/exercise3.test.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-higher-order-functions-reduce/exercicio/tests/exercise3.test.js
@@ -1,5 +1,9 @@
 const averageAge = require('../src/exercise3');
+const books = require('../src/books');
 
 describe('Teste para o exercise3: ', () => {
-  fail('Teste vazio!!!');
+  it('Verifica se a função calcula a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados', () => {
+    const expectedResult = 43;
+    expect(averageAge(books)).toEqual(expectedResult);
+  });
 });
