e32450aa5f4c71d50771b9ae3ab4428124326e1b
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-11 22:22:51 -0300

    bloco 8.3 - feito teste do exercise 05

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/tests/exercise5.test.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/tests/exercise5.test.js
@@ -1,5 +1,14 @@
 const fantasyOrScienceFictionAuthors = require('../src/exercise5');
+const books = require('../src/template-library');
 
 describe('Teste do exercise5: ', () => {
-  fail('teste vazio!!!');
+  it('Verifica se a função retorna um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia', () => {
+    const expectedResult = [
+      'Frank Herbert',
+      'George R. R. Martin',
+      'Isaac Asimov',
+      'J. R. R. Tolkien',
+    ];
+    expect(fantasyOrScienceFictionAuthors(books)).toEqual(expectedResult);
+  });
 });
