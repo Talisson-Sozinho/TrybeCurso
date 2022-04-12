49afc95065d66023546496a7229957c95e098b18
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-11 22:32:41 -0300

    bloco 8.3 - feito teste do exercise 07

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/tests/exercise7.test.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/tests/exercise7.test.js
@@ -1,5 +1,7 @@
 const authorWith3DotsOnName = require('../src/exercise7');
 
 describe('Teste do exercise7: ', () => {
-  fail('teste vazio!!!');
+  it('Verifica se a função retorna o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais', () => {
+    expect(authorWith3DotsOnName()).toBe('O Senhor dos Anéis');
+  });
 });
