37deb109aa6996925d73fbd2cf3534e194b54809
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-12 20:05:47 -0300

    bloco 8.4 - feito o teste do exercise5

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-higher-order-functions-reduce/exercicio/tests/exercise5.test.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-higher-order-functions-reduce/exercicio/tests/exercise5.test.js
@@ -1,5 +1,13 @@
 const containsA = require('../src/exercise5');
 
 describe('Teste para o exercise5: ', () => {
-  fail('Teste vazio!!!');
+  it('Verifica se dado um array de nomes, a função retorna a quantidade de vezes em que aparecem uma determinada letra', () => {
+    const names = [
+      'Aanemarie', 'Adervandes', 'Akifusa',
+      'Abegildo', 'Adicellia', 'Aladonata',
+      'Abeladerco', 'Adieidy', 'Alarucha',
+    ];
+    const word = 'A';
+    expect(containsA(names, word)).toBe(21);
+  });
 });
