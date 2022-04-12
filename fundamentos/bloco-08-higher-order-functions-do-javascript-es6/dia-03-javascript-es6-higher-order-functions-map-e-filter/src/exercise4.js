c4a2702f2dd060356f20d760c96f8e5897809814
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-12 00:12:45 -0300

    bloco 8.3 - feito exercise4 - função "oldBooksOrdered"

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/src/exercise4.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/src/exercise4.js
@@ -1,5 +1,8 @@
-function oldBooksOrdered() {
-  // escreva seu código aqui
+const currentYear = 2022;
+
+function oldBooksOrdered(books, moreThanYearsAgo) {
+  const booksFiltered = books.filter((book) => currentYear - book.releaseYear > moreThanYearsAgo);
+  return booksFiltered.sort((book1, book2) => book1.releaseYear - book2.releaseYear);
 }
 
 module.exports = oldBooksOrdered;
