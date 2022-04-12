3adbb6a1ecda202245d077e297535ceb630e190b
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-12 20:10:10 -0300

    bloco 8.4 - feito o teste do exercise6

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-higher-order-functions-reduce/exercicio/tests/exercise6.test.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-higher-order-functions-reduce/exercicio/tests/exercise6.test.js
@@ -1,5 +1,15 @@
 const studentAverage = require('../src/exercise6');
 
 describe('Teste para o exercise6: ', () => {
-  fail('Teste vazio!!!');
+  it('Verifica se dado dois array de dados relacionados, a função retorna um objeto com os dados pedido', () => {
+    const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+    const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+    const expected = [
+      { name: 'Pedro Henrique', average: 7.8 },
+      { name: 'Miguel', average: 9.2 },
+      { name: 'Maria Clara', average: 8.8 },
+    ];
+    expect(studentAverage(students, grades)).toEqual(expected);
+  });
 });
