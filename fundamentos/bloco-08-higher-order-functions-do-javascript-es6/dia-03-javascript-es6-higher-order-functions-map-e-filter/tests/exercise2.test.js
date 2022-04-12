fbea70dc49807734c7e42901ef0324dd81f844b2
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-11 22:11:35 -0300

    bloco 8.3 - feito teste do exercise 02

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/tests/exercise2.test.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/tests/exercise2.test.js
@@ -1,5 +1,16 @@
-const nameAndAge = require('../src/exercise2');
+const nameAndAge = require("../src/exercise2");
+const books = require("../src/template-library");
 
-describe('Teste do exercise2: ', () => {
-  fail('teste vazio!!!');
+describe("Teste do exercise2: ", () => {
+  it('verifica se a função retorna um objeto com a idade e o nome do autor em ordem crescente de idade', () => {
+    const expectedResult = [
+      { age: 31, author: "Isaac Asimov" },
+      { age: 38, author: "H. P. Lovecraft" },
+      { age: 39, author: "Stephen King" },
+      { age: 43, author: "George R. R. Martin" },
+      { age: 45, author: "Frank Herbert" },
+      { age: 62, author: "J. R. R. Tolkien" },
+    ];
+    expect(nameAndAge(books)).toEqual(expectedResult);
+  });
 });
