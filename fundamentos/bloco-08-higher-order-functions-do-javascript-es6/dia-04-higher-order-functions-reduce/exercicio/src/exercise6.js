404da4c66cb35b84004e8b1d7f97ed3390faeb72
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-13 00:21:23 -0300

    bloco 8.4 - feito exercise6

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-higher-order-functions-reduce/exercicio/src/exercise6.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-higher-order-functions-reduce/exercicio/src/exercise6.js
@@ -1,5 +1,11 @@
-function studentAverage() {
-  // escreva seu código aqui
+function studentAverage(students, grades) {
+  return students.map((student, index) => {
+    const newStudentFormat = {
+      name: student,
+      average: grades[index].reduce((acc, curr) => acc + curr)/grades[index].length,
+    }
+    return newStudentFormat;
+  });
 }
 
 module.exports = studentAverage;
