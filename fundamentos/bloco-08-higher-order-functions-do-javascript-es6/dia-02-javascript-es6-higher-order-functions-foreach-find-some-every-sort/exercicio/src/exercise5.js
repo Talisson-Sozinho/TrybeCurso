7e4b4882560c8d5787f2cff82641bdfbeb068a18
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-14 00:23:51 -0300

    bloco 8.2 - feito exercise5

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio/src/exercise5.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio/src/exercise5.js
@@ -1,5 +1,6 @@
-function everyoneWasBornOnSecXX() {
+function everyoneWasBornOnSecXX(books) {
   // escreva seu código aqui
+  return books.every(({author:{birthYear}}) => birthYear > 1900 && birthYear <= 2000);
 }
 
 module.exports = everyoneWasBornOnSecXX;
