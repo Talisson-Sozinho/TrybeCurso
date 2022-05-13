f2bdd4d62f1bd8b908a7ee65b85b5e4b88e5a311
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-05-13 18:52:13 -0300

    bloco 11.2 - feito para fixar requisito 03

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/react-forms/src/components/Form/index.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/react-forms/src/components/Form/index.jsx
@@ -4,30 +4,28 @@ export class Form extends React.Component {
   constructor() {
     super();
     this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      selectValue: '',
-      inputTextValue: '',
-      inputCheckboxValue: '',
-      textareaValue: '',
-    }
+    this.state = {};
   }
 
-  handleChange(event) {
-    this.setState({
-      inputTextValue: event.target.checked,
+  async handleChange({target}) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    await this.setState({
+      [name]: value,
     })
+    console.log(this.state)
   }
   render() {
     return (
       <form>
-        <select>
+        <select name='inputOptions' value={this.state.inputOptions} onChange={this.handleChange}>
           <option>hm</option>
           <option>haha</option>
           <option>kkkkkk</option>
         </select>
-        <input type="text" value={this.state.inputTextValue} onChange={this.handleChange} />
-        <input type="checkbox"/>
-        <textarea></textarea>
+        <input type="text" name='inputText' value={this.state.inputText} onChange={this.handleChange} />
+        <input type="checkbox" name='inputCheckbox' value={this.state.inputCheckbox} onChange={this.handleChange} />
+        <textarea name='inputTextarea' value={this.state.inputTextarea} onChange={this.handleChange}></textarea>
       </form>
     );
   }
